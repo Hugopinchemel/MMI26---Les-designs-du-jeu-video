8bedcfc81bf00eda3b73e3227aec8c56377a3fac
Hugo Pinchemel <148193347+Hugopinchemel@users.noreply.github.com>
2024-04-24 16:45:35 +0200

    traps

--- Assets/Scripts/traps/traps.cs
+++ Assets/Scripts/traps/traps.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enteraroom : MonoBehaviour
+public class traps : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
